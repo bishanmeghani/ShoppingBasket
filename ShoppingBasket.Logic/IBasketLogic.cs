a3e50e21441482b8db568338172efb5e88529261
bishanmeghani <bishan_rulez@hotmail.com>
2018-06-20 12:51:59 +0100

    Created Basket Logic

---
+++ ShoppingBasket.Logic/IBasketLogic.cs
@@ -0,0 +1,13 @@
+﻿using ShoppingBasket.DataAccessLayer;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingBasket.Logic
+{
+    public interface IBasketLogic
+    {
+        void AddProductToBasket(Product product, Basket basket);
+        decimal CalculateTotalCostOfBasket(Basket basket);
+    }
+}
