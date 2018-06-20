a56fceab81a89578a10b673da13d56fe068dbd60
bishanmeghani <bishan_rulez@hotmail.com>
2018-06-20 13:32:20 +0100

    Changed Basket Logic

--- ShoppingBasket.Logic/BasketLogic.cs
+++ ShoppingBasket.Logic/BasketLogic.cs
@@ -1,8 +1,34 @@
 ﻿using System;
+using ShoppingBasket.DataAccessLayer;
+using System.Collections.Generic;
 
 namespace ShoppingBasket.Logic
 {
-    public class Class1
+    public class BasketLogic : IBasketLogic
     {
+        public StockDb stockDb;
+
+        public BasketLogic()
+        {
+            stockDb = new StockDb();
+        }
+
+        public void AddProductToBasket(Product product, Basket basket)
+        {
+            basket.ListOfProducts.Add(product);
+        }
+
+        public decimal CalculateTotalCostOfBasket(Basket basket)
+        {
+            decimal total = 0.0m;
+            
+            basket.TotalPriceEachItem.Add("A", 0.00m);
+            basket.TotalPriceEachItem.Add("B", 0.00m);
+            basket.TotalPriceEachItem.Add("C", 0.00m);
+            basket.TotalPriceEachItem.Add("D", 0.00m);
+
+            Dictionary<string, int> itemRecord = new Dictionary<string, int>();
+            return total;
+        }
     }
 }
